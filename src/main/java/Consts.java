e54afab5a5a535bb71140f79e4178d4dfc4a874e
andriipysakov <standpolska@gmail.com>
2023-03-29 17:19:39 -0400

    Upgrade

--- src/main/java/Consts.java
+++ src/main/java/Consts.java
@@ -4,5 +4,8 @@ public class Consts {
     public static final String TOKEN = "?apikey=jFKLAlpACfBuf3ONSFrV7k4pjYxCu4bt";
     public static final String LIVE_ENDPOINT = "/live";
     public static final String HISTORICAL_ENDPOINT = "/historical";
+    public static final String AMPERSAND = "&";
+    public static final String SOURCE_DEFAULT = "source=USD";
+    public static final String CURRENCIES_DEFAULT = "currencies=EUR%2CCAD%2CNIS%2CRUB";
 
 }
--- src/test/java/CurrencyResponseTest.java
+++ src/test/java/CurrencyResponseTest.java
@@ -7,9 +7,11 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class CurrencyResponseTest {
 
+    public static Response response;
+
     @Test
     public void getCurrencyResponseCodeTest(){
-        Response response = given().contentType("application/json").get(Consts.URL + Consts.LIVE_ENDPOINT + Consts.TOKEN);
+        response = given().contentType("application/json").get(Consts.URL + Consts.LIVE_ENDPOINT + Consts.TOKEN);
         response.then().statusCode(200);
         response.then().body("success", equalTo(true));
         response.then().body("timestamp", notNullValue());
@@ -21,5 +23,24 @@ public class CurrencyResponseTest {
 
     }
 
-    
+    @ParameterizedTest
+    @ValueSource (strings = {"USDEUR", "USDNIS","USDRUB","USDCAD"})
+    public void getCurrenciesResponseTest(String currencies){
+        response = given().contentType("application/json").get(Consts.URL + Consts.LIVE_ENDPOINT + Consts.TOKEN + Consts.AMPERSAND + Consts.SOURCE_DEFAULT + Consts.AMPERSAND + Consts.CURRENCIES_DEFAULT);
+        response.then().statusCode(200);
+        response.then().body("success", equalTo(true));
+        response.then().body("timestamp", notNullValue());
+        response.then().body("source", equalTo("USD"));
+        response.then().body("quotes." + currencies, notNullValue());
+    }
+
+    @Test
+    public void getCurrenciesResponseNegativeTest(){
+        response = given().contentType("application/json").get(Consts.URL + Consts.LIVE_ENDPOINT + Consts.TOKEN + Consts.AMPERSAND + Consts.SOURCE_DEFAULT + Consts.AMPERSAND + "currencies=NIS");
+        response.then().statusCode(200);
+        response.then().body("success", equalTo(false));
+        response.then().body("error.code", equalTo(202));
+        response.then().body("error.info", containsString("You have provided one or more invalid Currency Code"));
+    }
+
 }
