60695ba908ff138e7d9280b2ab099f56e8a77966
andriipysakov <standpolska@gmail.com>
2023-03-29 22:23:41 -0400

    Upgrade

--- src/test/java/HistoricalResponseTest.java
+++ src/test/java/HistoricalResponseTest.java
@@ -1,7 +1,5 @@
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
