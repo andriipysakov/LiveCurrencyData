e346e5780980fab1bac1fff9cc9c1dc5718ee7fe
andriipysakov <standpolska@gmail.com>
2023-03-29 22:16:32 -0400

    Upgrade

--- src/test/java/HistoricalResponseTest.java
+++ src/test/java/HistoricalResponseTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class HistoricalResponseTest {
 
     public static Response response;
@@ -16,12 +18,12 @@ public class HistoricalResponseTest {
         response.then().body("success", equalTo(true));
         response.then().body("historical", equalTo(true));
         response.then().body("date", equalTo("2018-01-01"));
-        response.then().body("timestamp", notNullValue());
+        response.then().body("timestamp", equalTo(1514851199));
         response.then().body("source", equalTo("USD"));
-        response.then().body("quotes.USDCAD", notNullValue());
-        response.then().body("quotes.USDEUR", notNullValue());
+        response.then().body("quotes.USDCAD", equalTo(1.25551f));
+        response.then().body("quotes.USDEUR", equalTo(0.832296f));
         response.then().body("quotes.USDNIS", notNullValue());
-        response.then().body("quotes.USDRUB", notNullValue());
+        response.then().body("quotes.USDRUB", equalTo(57.980701f));
     }
 
     @Test
@@ -40,6 +42,15 @@ public class HistoricalResponseTest {
         response.then().body("success", equalTo(false));
         response.then().body("error.code", equalTo(302));
         response.then().body("error.info", containsString("You have entered an invalid date"));
+    }
+
+    @Test
+    public void getHistoricalInvalidCurrenciesResponseNegativeTest(){
+        response = given().contentType("application/json").get(Consts.URL + Consts.HISTORICAL_ENDPOINT + Consts.TOKEN + Consts.AMPERSAND + Consts.DATE_DEFAULT + Consts.AMPERSAND + "currencies=NIS");
+        response.then().statusCode(200);
+        response.then().body("success", equalTo(false));
+        response.then().body("error.code", equalTo(202));
+        response.then().body("error.info", containsString("You have provided one or more invalid Currency Code"));
     }
 
 }
